ace6bca515fab1e7f654c8006eb7f3675f6186b4
xidoWeb <erivery2@gmail.com>
2016-05-04 12:10:48 +0900

    rwd_02

--- study/step_08_rwd/js/rwd.js
+++ study/step_08_rwd/js/rwd.js
@@ -6,16 +6,52 @@ var winResize = function(){
       winWidth =win.width();
   
     if(winWidth <= 640){
-      $("body").css({"background-color":"#456"});
+     var rwdhead = $('#rwdHead').find('h1'),
+         rwdheadWidth = rwdhead.width();
+      
+          rwdhead.css({'margin-left': -rwdheadWidth / 2 });
+      
+      var mapBtn = $('.map_btn'),
+          mapH2 = mapBtn.children('h2');        
+      
+          mapH2.on('click', function(){
+            var $this = $(this),
+                  mapUl = $this.next('ul');
+                mapUl.stop(true, true).toggleClass('side', 500);
+          });     
+
     }else if(winWidth > 641 && winWidth <= 1024){
-      $("body").css({"background-color":"#951"});
+      if(winWidth < 705){
+        $("#gnb").css({float:'right'});
+        $("#sitemap").css({marginTop: -80, marginLeft:'5px'});
+      }
+      
     }else{
-      $("body").css({"background-color":"#159"});
+     
     }
   $("body")
-     .html("<p>현재 브라우저 가로값은" + win.width() + "입니다.</p>" )
-     .find("p").css({"color":"#fff", "font-size":"1.5rem"});
+//     .html("<p>현재 브라우저 가로값은" + win.width() + "입니다.</p>" )
+//     .find("p").css({"color":"#fff", "font-size":"1.5rem"});
   };
   winResize();
-  win.on("resize", winResize);
+  
+  var winWidth = $(window).width();
+  win.on('resize', function(){
+      if($(window).width() != winWidth){
+          location.reload();
+        }
+     winResize();
+  })
+//  win.on("resize", winResize);
 });
+
+
+
+
+
+
+
+
+
+
+
